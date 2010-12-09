6e12e877517b52e3e8db97196c3b739ae528dd61
Pedro Teixeira <pedro.teixeira@gmail.com>
2010-12-09 15:51:14 +0000

    detach keyMap from db command

--- test/test_meta.js
+++ test/test_meta.js
@@ -1,37 +1,46 @@
 var assert     = require('assert');
 
+var DB_PATH = __dirname + '/../tmp/db';
+
 module.exports.run = function(next) {
   var alfred = require('../lib/alfred');
-  alfred.open('tmp/db', function(err, db) {
-    if (err) {
-      throw err;
-    }
+  alfred.open(DB_PATH, function(err, db) {
+    if (err) { throw err; }
     db.attach_key_map('users', null, function(err, key_map) {
-      if (err) {
-        throw err;
-      }
+      if (err) { throw err; }
       db.users.put('abc', 'def', function(err) {
-        if (err) {
-          throw err;
-        }
+        if (err) { throw err; }
         db.users.get('abc', function(err, value) {
+          if (err) { throw err; }
           assert.equal(value, 'def');
         });
         
         db.close(function(err) {
-          if (err) {
-            throw err;
-          }
-          
+          if (err) { throw err; }
+
           // Reopen database and see if users key map is still there
-          alfred.open('tmp/db', function(err, db) {
-            if (err) {
-              throw err;
-            }
+          alfred.open(DB_PATH, function(err, db) {
+            if (err) { throw err; }
             assert.ok(!!db.users, 'db.users no longer exists');
             db.users.get('abc', function(err, value) {
               assert.equal(value, 'def');
-              next();
+              
+              // Detach
+              db.detach_key_map('users', function(err) {
+                if (err) { throw err; }
+                assert.ok(!!!db.users);
+                
+                db.close(function(err) {
+                  if (err) { throw err; }
+                  alfred.open(DB_PATH, function(err, db) {
+                    if (err) { throw err; }
+                    
+                    assert.ok(!!!db.users);
+                    
+                    next();
+                  });
+                });
+              });
             });
           });
         })
