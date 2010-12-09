29e6c0f74619f1b44120857598485821b2daf416
Pedro Teixeira <pedro.teixeira@gmail.com>
2010-12-09 17:56:18 +0000

    meta indexes

---
+++ test/test_meta_indexes.js
@@ -0,0 +1,44 @@
+var assert     = require('assert');
+
+var DB_PATH = __dirname + '/../tmp/db';
+
+module.exports.run = function(next) {
+  var alfred = require('../lib/alfred');
+  alfred.open(DB_PATH, function(err, db) {
+    if (err) { throw err; }
+    
+    var done = function(err) {
+      if (err) { throw err; }
+      var transform_function = function(record) {
+        return record;
+      };
+      assert.ok(!!db.users);
+      db.users.addIndex('idx', {ordered : true}, transform_function, function(err) {
+        if (err) { throw err; }
+        assert.ok(!!db.users.idx);
+        
+        db.users.put('abc', 'def', function(err) {
+          if (err) { throw err; }
+          var match_count = 0;
+          db.users.idx.indexMatch('def', function(err, value) {
+            if (err) { throw err; }
+            match_count ++;
+            assert.equal(1, match_count, 'more than one match');
+            if (err) { throw err; }
+            db.close(function(err) {
+              if (err) { throw err; }
+              next();
+            });
+          });
+        });
+        
+      });
+    }
+    
+    if (!db.users) {
+      db.attach_key_map('users', done);
+    } else {
+      done(null);
+    }
+  });
+};
