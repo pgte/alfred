25c8ce40751aa31370c2e9cb65bc5e7926b6ecde
Pedro Teixeira <pedro.teixeira@gmail.com>
2010-12-14 17:21:44 +0000

    testing composing operatord

---
+++ test/operators/test_composed.js
@@ -0,0 +1,70 @@
+var assert     = require('assert');
+
+var DB_PATH = __dirname + '/../../tmp/db';
+
+var USERS = {
+    1: {name: 'Pedro', age: 35}
+  , 2: {name: 'John', age: 32}
+  , 3: {name: 'Bruno', age: 28}
+  , 4: {name: 'Sandra', age: 35}
+  , 5: {name: 'Patricia', age: 42}
+  , 6: {name: 'Joana', age: 29}
+};
+
+var USER_COUNT = 6;
+
+module.exports.run = function(next) {
+  var alfred = require('../../lib/alfred');
+  
+  var timeout = setTimeout(function() {
+    throw new Error('timeout');
+  }, 5000);
+  
+  alfred.open(DB_PATH, function(err, db) {
+    if (err) { throw err; }
+    db.ensure_key_map_attached('users', null, function(err) {
+      if (err) { throw err; }
+      
+      var transform_function = function(user) {
+        return user.age;
+      };
+      
+      db.users.ensureIndex('age', {ordered: true}, transform_function, function(err) {
+        if (err) { throw err; }
+        
+        var users_in = 0;
+        for (var id in USERS) {
+          if (USERS.hasOwnProperty(id)) {
+            (function(id) {
+              var user = USERS[id];
+              db.users.put(id, user, function(err) {
+                if (err) { throw err; }
+                users_in ++;
+                if (users_in == USER_COUNT) {
+                  // all users done
+                  
+                  var users_found = 0;
+                  
+                  db.users.find({'age' : {$gt: 29, $lte: 35}}, function(err, key, value) {
+                    if (err) { throw err; }
+                    assert.deepEqual(value, USERS[key]);
+                    assert.ok(value.age > 29 && value.age <= 35, 'age is not equal to > 29 and < 35 for found user with key ' + key);
+                    users_found ++;
+                    assert.ok(users_found <= 3, 'already found ' + users_found + ' users');
+                    if (users_found == 3) {
+                      clearTimeout(timeout);
+                      db.close(function(err) {
+                        if (err) { throw err; }
+                        next();
+                      })
+                    }
+                  });
+                }
+              });
+            })(id);
+          }
+        }
+      });
+    })
+  });
+};
